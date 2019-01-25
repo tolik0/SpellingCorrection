8c1283d5f8189380e5aa2f2d2034c7c05e28ea69
ostapViniavskyi <35109193+ostapViniavskyi@users.noreply.github.com>
2019-01-25 11:47:16 +0200

    added correction algorothm based on embedding

---
+++ scripts/embeddingCorrector.py
@@ -0,0 +1,48 @@
+from jellyfish import damerau_levenshtein_distance
+from scripts.predict_from_context import predict_output_word
+import copy
+
+
+class EmbeddingCorrector:
+
+    def __init__(self, model=None):
+        """Initialize EmbeddingCorrector instance
+        
+        Args:
+            model (gensim.models.base_any2vec.BaseWordEmbeddingsModel): gensim model with pretrained words embeddings
+  
+        """
+        self.model = model
+
+    def correct_sentence(self, sentence, window=2, topn=10):
+        """Correct mistakes in a single sentence
+        
+        Args: 
+            sentence (:obj:`list` of :obj:`str`): list of tokens in the sentence
+            window (int): word window used to predict center word from context
+            topn (int): number of most probable candidates to choose from
+        Returns: 
+            
+        """
+        sentence = copy.deepcopy(sentence)
+        for i in range(len(sentence)):
+            if sentence[i] not in self.model.wv.vocab:
+                candidates = predict_output_word(self.model, sentence[max(0, i - window): i] +
+                                                 sentence[i + 1: min(len(sentence) - 1, i + window + 1)], topn=topn)
+                sentence[i] = min(candidates, key=lambda x: damerau_levenshtein_distance(x[0], sentence[i]))[0]
+        return sentence
+
+    def correct_text(self, text, window=2, topn=10):
+        """Correct mistakes in text
+
+        Args:
+            text (:obj:`list` of :obj:`list` of :obj:`str`): list of lists of tokens in the sentence
+            window (int): word window used to predict center word from context
+            topn (int): number of most probable candidates to choose from
+        Returns:
+
+        """
+        text = copy.deepcopy(text)
+        for i in range(len(text)):
+                text[i] = self.correct_sentence(text[i], window=window, topn=topn)
+        return text
---
+++ scripts/predict_from_context.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def predict_output_word(model, context_words_list, topn=10):
+    """Get the probability distribution of the center word given context words.
+
+        Parameters
+        ----------
+        model: gensim.models.base_any2vec.BaseWordEmbeddingsModel
+            Model that predicts the output
+        context_words_list : list of str
+            List of context words.
+        topn : int, optional
+            Return `topn` words and their probabilities.
+
+        Returns
+        -------
+        list of (str, float)
+            `topn` length list of tuples of (word, probability).
+
+    """
+    if not model.negative:
+        raise RuntimeError(
+        "We have currently only implemented predict_output_word for the negative sampling scheme, "
+        "so you need to have run word2vec with negative > 0 for this to work."
+        )
+
+    if not hasattr(model.wv, 'vectors') or not hasattr(model.trainables, 'syn1neg'):
+        raise RuntimeError("Parameters required for predicting the output words not found.")
+
+    word_vocabs = [model.wv.vocab[w] for w in context_words_list if w in model.wv.vocab]
+    if not word_vocabs:
+        print("All the input context words are out-of-vocabulary for the current model.")
+        return None
+
+    word2_indices = [word.index for word in word_vocabs]
+
+    l1 = np.sum(model.wv.vectors[word2_indices], axis=0) / 5
+    if word2_indices and model.cbow_mean:
+        l1 /= len(word2_indices)
+
+    # propagate hidden -> output and take softmax to get probabilities
+    prob_values = np.exp(np.dot(l1, model.trainables.syn1neg.T))
+    prob_values /= np.sum(prob_values)
+    top_indices = np.argsort(prob_values)[-topn:][::-1]
+    # returning the most probable output words with their probabilities
+    return [(model.wv.index2word[index1], prob_values[index1]) for index1 in top_indices]
