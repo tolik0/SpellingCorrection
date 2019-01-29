f36f348cd49b561866f3868345805c3474083e4a
ostapViniavskyi <35109193+ostapViniavskyi@users.noreply.github.com>
2019-01-29 22:11:14 +0200

    added inference model

---
+++ scripts/attention_utils.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import RepeatVector
+
+
+def softmax(x, axis=1):
+    """Softmax activation function.
+    # Arguments
+        x : Tensor.
+        axis: Integer, axis along which the softmax normalization is applied.
+    # Returns
+        Tensor, output of softmax transformation.
+    # Raises
+        ValueError: In case `dim(x) == 1`.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
+        
+
+def one_hot(x):
+    x = K.argmax(x)
+    x = tf.one_hot(x, 56) 
+    x = RepeatVector(1)(x)
+    return x
